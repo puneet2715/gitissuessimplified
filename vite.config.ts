6d3aaa05e5834437100bf66a7a461eec24c919b3
Puneet <puneet2715@gmail.com>
2022-12-14 17:37:08 +0530

    minor fix in deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gitissuessimplified",
   plugins: [react()],
   resolve: {
     alias: {
-      'node-fetch': 'isomorphic-fetch',
-    }
+      "node-fetch": "isomorphic-fetch",
+    },
   },
 });
